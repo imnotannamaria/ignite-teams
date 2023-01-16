e8b2560a1b7f794322aa7d109a9f3d11428fbe19
Anna Maria <aannamariabr@outlook.com>
2023-01-16 11:30:49 -0300

    Resolvendo conflitos de tipagem do styled components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,8 +2,11 @@ import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
 
 export function Groups() {
+  const [ groups, setGroups ] = useState(['Galera da Rocket']);
+  
   return (
     <Container>
       <Header />
